01928ffd4d71dfb36d47758f9879010b14a2e54d
Suvam Ghosh <suvam25@kgpian.iitkgp.ac.in>
2026-01-15 16:46:43 +0530

    made the about section reactive for mobile

--- data/project.js
+++ data/project.js
@@ -11,8 +11,11 @@ export let projectDetails=[
    image: `Project2.png`
 },
 {
-   title:'Accessible Voice Form Filler',
-   body: `A social innovation project designed to bridge the digital divide. It allows elderly or disabled users to fill out complex digital forms using only voice commands, processed via Natural Language Processing (NLP) to ensure accuracy.`,
+   title:' Weather Dashboard',
+   body: `A website where users can enter a city name and get the current weather, humidity, and wind speed. This teaches you how to fetch data from an API (like OpenWeatherMap) and display it, which is the exact same skill needed for the Gemini API project later.`,
    image: `Project3.png`
 }
 ];
+export const aboutMe=[`I am an undergraduate student in Manufacturing Science & Engineering at IIT Kharagpur, driven by a curiosity-first approach to learning and problem-solving. My academic journey has been shaped by a strong inclination toward understanding how things work beneath the surface—whether it’s engineering systems, computational logic, or decision-making frameworks. I enjoy exploring beyond formal coursework, constantly pushing myself to connect concepts across disciplines rather than viewing them in isolation.
+               `,`My approach to problems is grounded in strong fundamentals, structured reasoning, and systems thinking. I believe good solutions emerge from breaking complex problems into simpler components, analyzing constraints, and designing with scalability in mind. With a solid foundation in data structures, algorithms, and Java-based problem solving, I focus not just on getting correct answers, but on building efficient, elegant, and explainable solutions. I value clarity in thought, precision in execution, and learning through iteration.`,
+`What motivates me most is working on problems that have depth, long-term relevance, and real impact. I am particularly interested in areas that lie at the intersection of engineering, research, and entrepreneurship, where rigorous thinking meets practical application. Whether through advanced technical work, research-oriented exploration, or building scalable ideas from the ground up, my goal is to create work that is meaningful, well-thought-out, and capable of making a difference beyond the immediate context.`]
--- script/body.js
+++ script/body.js
@@ -1,4 +1,9 @@
 import { projectDetails } from "../data/project.js";
+
+const aboutMe=[`I am an undergraduate student in Manufacturing Science & Engineering at IIT Kharagpur, driven by a curiosity-first approach to learning and problem-solving. My academic journey has been shaped by a strong inclination toward understanding how things work beneath the surface—whether it’s engineering systems, computational logic, or decision-making frameworks. I enjoy exploring beyond formal coursework, constantly pushing myself to connect concepts across disciplines rather than viewing them in isolation.
+               `,`My approach to problems is grounded in strong fundamentals, structured reasoning, and systems thinking. I believe good solutions emerge from breaking complex problems into simpler components, analyzing constraints, and designing with scalability in mind. With a solid foundation in data structures, algorithms, and Java-based problem solving, I focus not just on getting correct answers, but on building efficient, elegant, and explainable solutions. I value clarity in thought, precision in execution, and learning through iteration.`,
+`What motivates me most is working on problems that have depth, long-term relevance, and real impact. I am particularly interested in areas that lie at the intersection of engineering, research, and entrepreneurship, where rigorous thinking meets practical application. Whether through advanced technical work, research-oriented exploration, or building scalable ideas from the ground up, my goal is to create work that is meaningful, well-thought-out, and capable of making a difference beyond the immediate context.`]
+
 generateProject();
 function generateProject()
 {
@@ -18,4 +23,45 @@ function generateProject()
             `
       });
 }
+
+readmore();
+ function readmore()
+{
+       const about=document.querySelector('.js-aboutMe');
+       console.log(window.innerWidth);
+       about.innerHTML='';
+       if(window.innerWidth<=700)
+       {
+            for(var i=0;i<1;i++)
+            {
+                about.innerHTML+=`<div class="about-section-content">${aboutMe[i]}</div>`;
+            }
+            about.innerHTML+=`<button class="readmore"  > &#8595; </button>`;
+            const readmorebtn=document.querySelector('.readmore');
+            readmorebtn.addEventListener('click',expansion);
+            
+       }
+       else
+       {
+           aboutMe.forEach((item,i) =>
+           {
+               about.innerHTML+=`<div class="about-section-content">${item}</div>`;
+           })
+       }
+      } 
+
+ function expansion(){
+            const about=document.querySelector('.js-aboutMe');
+            console.log(about);
+            about.innerHTML='';
+            aboutMe.forEach((item,i) =>
+            {
+                about.innerHTML+=`<div class="about-section-content">${item}</div>`;
+            })
+            about.innerHTML+=`<button class="readmore"  > &#8593; </button>`;
+            const readmorebtn=document.querySelector('.readmore');
+            readmorebtn.addEventListener('click',readmore);
+       }
+
+
 
